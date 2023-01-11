2fae41a080ba08f77ed8c019628e9431457cccd1
Angel M De Miguel <dangel@vmware.com>
2023-01-11 08:32:06 +0100

    fix: expose only the Routes struct as the entrypoint for the router

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,9 +1,8 @@
 // Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod files;
-pub mod route;
-pub mod routes;
+mod files;
+mod route;
+mod routes;
 
-pub use route::Route;
-pub use routes::Routes;
+pub(crate) use routes::Routes;
