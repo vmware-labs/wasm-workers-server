d0b2ae1747883848a3ec45dc0a3221c33c7ce8e9
Kartikey Rawat <41143496+carrycooldude@users.noreply.github.com>
2023-05-27 12:51:12 +0530

    feat: added error message and exposes WASM error message (#141)
    * Updated the error msg with eprintln!

--- crates/server/src/handlers/worker.rs
+++ crates/server/src/handlers/worker.rs
@@ -8,7 +8,7 @@ use actix_web::{
     web::{Bytes, Data},
     HttpRequest, HttpResponse,
 };
-use std::{fs::File, sync::RwLock};
+use std::{fs::File, io::Write, sync::RwLock};
 use wws_router::Routes;
 use wws_worker::io::WasmOutput;
 
@@ -75,7 +75,20 @@ pub async fn handle_worker(req: HttpRequest, body: Bytes) -> HttpResponse {
                 .run(&req, &body_str, store, vars, stderr_file.get_ref())
             {
                 Ok(output) => (output, true),
-                Err(_) => (WasmOutput::failed(), false),
+                Err(error) => {
+                    if let Some(stderr_file) = stderr_file.get_ref() {
+                        if let Ok(mut stderr_file) = stderr_file.try_clone() {
+                            stderr_file
+                                .write_all(error.to_string().as_bytes())
+                                .expect("Failed to write error to stderr_file");
+                        } else {
+                            eprintln!("{}", error);
+                        }
+                    } else {
+                        eprintln!("{}", error);
+                    }
+                    (WasmOutput::failed(), false)
+                }
             };
 
         let mut builder = HttpResponse::build(
