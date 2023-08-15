04610ff5cd58a065c50cf661214a02ee0e7fb9d9
Christoph Voigt <c.voigt@reply.de>
2023-08-15 17:22:34 +0200

    prepare kv example

--- kits/zig/worker/build.zig
+++ kits/zig/worker/build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const package_name = "worker";
 const package_path = "src/worker.zig";
 
-const examples = [2][]const u8{ "main", "basic" };
+const examples = [3][]const u8{ "main", "basic", "worker-kv" };
 // const examples = [4][]const u8{ "main", "basic", "json", "output" };
 
 pub fn build(b: *std.Build) void {
