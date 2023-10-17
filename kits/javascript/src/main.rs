ed0bfacbec2e00e64f29d79e273be923878f4dc0
Ángel M <dangel@vmware.com>
2023-10-17 18:37:27 +0200

    feat: add support for ECMAScript modules in the JavaScript kit (#236)
    * feat: add support for ECMAScript modules in the JavaScript kit
    
    * fix: remove unnecesary exclude in GitHub actions
    
    * feat: improve module detection in the JavaScript kit
    
    * feat: update JS Hono example to use modules and minify the output
    
    * fix: detect exported objects as JS modules
    
    * chore: stop using unicode in regexps and fix block one
    
    * fix: use differnt line endings so modules are properly detected on Windows
    
    * feat: add a new e2e for js-hono example

--- kits/javascript/src/main.rs
+++ kits/javascript/src/main.rs
@@ -6,6 +6,7 @@ mod error;
 
 use bindings::load_bindings_into_global;
 use javy::{json, Runtime};
+use regex::Regex;
 use std::{
     env, fs,
     io::{stdin, stdout, Read, Write},
@@ -16,6 +17,34 @@ wit_bindgen_rust::import!({paths: ["../../wit/core/http.wit"]});
 
 // JS polyfill
 static POLYFILL: &str = include_str!("../shims/dist/index.js");
+
+/// Determine the worker JS type
+enum JSWorkerType {
+    /// Relies on the global scope. No ECMA modules.
+    Global,
+    /// Exports a default function which is the one replying to the events.
+    DefaultExport,
+}
+
+/// Identify the worker source code to run it properly.
+fn identify_type(src: &str) -> JSWorkerType {
+    // Detect default exported functions and objects
+    let default_regex = Regex::new(r"(?-u)export\s+default\s+\w+;?").unwrap();
+    // Detect default exported object
+    let default_block_regex = Regex::new(r"export\s+default\s*\{([\s\n\r]*.*)+\};?").unwrap();
+    // Detect exported functions with the "as" syntax like "export { app as default }";
+    let default_as_regex =
+        Regex::new(r"(?-u)export\s*\{[\s\n\r]*\w+\s+(as default){1}[\s\n\r]*\};?").unwrap();
+
+    if default_regex.is_match(src)
+        || default_block_regex.is_match(src)
+        || default_as_regex.is_match(src)
+    {
+        JSWorkerType::DefaultExport
+    } else {
+        JSWorkerType::Global
+    }
+}
 
 fn main() {
     let runtime = Runtime::default();
@@ -49,7 +78,25 @@ fn main() {
         },
     }
 
-    context.eval_global("handler.js", &contents).unwrap();
+    // Checks if the given code uses ECMAScript modules. Currently, we don't plan to integrate
+    // a full JavaScript parser for this, so we are going to rely on regexps. This implementation
+    // has limitations like detecting "// export default app;" as a module. In the future,
+    // we may add more complete checks.
+    match identify_type(&contents) {
+        JSWorkerType::DefaultExport => {
+            let _ = context.eval_module("handler.mjs", &contents).unwrap();
+            let _ = context
+                .eval_module(
+                    "runtime.mjs",
+                    &format!("import {{ default as handler }} from 'handler.mjs'; addEventListener('fetch', (e) => {{ e.respondWith(handler.fetch(e.request)) }});"),
+                )
+                .unwrap();
+        }
+        _ => {
+            context.eval_global("handler.js", &contents).unwrap();
+        }
+    }
+
     let global = context.global_object().unwrap();
     let entrypoint = global.get_property("entrypoint").unwrap();
 
