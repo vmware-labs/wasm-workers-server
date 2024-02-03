4956f9717925328ef40c4cf0ae7164ea9f992c98
sea-grass <dev.seagrass@gmail.com>
2024-02-03 13:20:16 -0500

    chore: simplify Zig SDK reading request/writing response

--- examples/zig-module/src/main.zig
+++ examples/zig-module/src/main.zig
@@ -1,6 +1,46 @@
 const std = @import("std");
 const wws = @import("wws");
 
+/// Receives an arena allocator such that all allocations made while handling
+/// the request can be easily freed by the caller
+fn handle(arena: *std.heap.ArenaAllocator, request: *const wws.Request) !wws.Response {
+    const allocator = arena.allocator();
+
+    // Prepare response body
+    var body = std.ArrayList(u8).init(allocator);
+    defer body.deinit();
+
+    try body.writer().print("{any} {s}\n", .{ request.method, request.url });
+
+    {
+        var it = request.kv.map.iterator();
+        while (it.next()) |entry| {
+            try body.writer().print("kv.{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        }
+    }
+
+    var counter: usize = if (request.kv.map.get("counter")) |v| try std.fmt.parseInt(usize, v, 10) else 0;
+
+    // Increment counter, save the result to storage
+    counter += 1;
+
+    // Prepare response
+    var response = wws.Response{
+        .data = try body.toOwnedSlice(),
+    };
+
+    try response.headers.map.put(allocator, "Content-Type", "text/plain");
+    {
+        var buf = std.ArrayList(u8).init(allocator);
+        defer buf.deinit();
+
+        try buf.writer().print("{d}", .{counter});
+        try response.kv.map.put(allocator, "counter", try buf.toOwnedSlice());
+    }
+
+    return response;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
@@ -11,64 +51,14 @@ pub fn main() !void {
     }
     const allocator = gpa.allocator();
 
-    const stdin = std.io.getStdIn();
-    var input = std.ArrayList(u8).init(allocator);
-    defer input.deinit();
-    try stdin.reader().readAllArrayList(&input, std.math.maxInt(usize));
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
 
-    std.debug.print("{s}\n", .{input.items});
+    const parse_result = try wws.parseStream(allocator, .{});
+    defer parse_result.deinit();
 
-    var result = try wws.parseStream(allocator, .{ .input_stream = .{ .bytes = input.items } });
-    defer result.deinit();
-    const wws_request = result.value;
-    std.debug.print("{s} {any}\n", .{ wws_request.url, wws_request.method });
-
-    // Prepare response
-    var body = std.ArrayList(u8).init(allocator);
-    defer body.deinit();
-
-    try body.writer().print("{any} {s}\n", .{ wws_request.method, wws_request.url });
-
-    {
-        var it = wws_request.kv.map.iterator();
-        while (it.next()) |entry| {
-            try body.writer().print("kv.{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
-        }
-    }
-
-    var headers = wws.Headers.init(allocator);
-    defer headers.deinit();
-    try headers.append("Content-Type", "text/plain");
-
-    var storage = std.StringHashMap([]const u8).init(allocator);
-    defer storage.deinit();
-    defer {
-        var it = storage.iterator();
-        while (it.next()) |entry| {
-            allocator.free(entry.value_ptr.*);
-        }
-    }
-
-    var counter: usize = if (wws_request.kv.map.get("counter")) |v| try std.fmt.parseInt(usize, v, 10) else 0;
-
-    // Increment counter, save the result to storage
-    counter += 1;
-
-    {
-        var buf = std.ArrayList(u8).init(allocator);
-        defer buf.deinit();
-        try buf.writer().print("{d}", .{counter});
-        try storage.put("counter", try buf.toOwnedSlice());
-    }
-
-    const response = try wws.formatResponse(allocator, .{
-        .data = body.items,
-        .status = 200,
-        .headers = &headers,
-        .storage = &storage,
-    });
-    defer allocator.free(response);
+    const response = try handle(&arena, &parse_result.value);
 
     const stdout = std.io.getStdOut();
-    try stdout.writer().print("{s}", .{response});
+    try wws.writeResponse(response, stdout.writer());
 }
--- kits/zig/wws/src/wws.zig
+++ kits/zig/wws/src/wws.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 
 pub const Method = std.http.Method;
-pub const Headers = std.http.Headers;
+
+pub const Request = struct {
+    url: []const u8,
+    method: Method,
+    body: []const u8,
+    headers: std.json.ArrayHashMap([]const u8),
+    kv: std.json.ArrayHashMap([]const u8),
+    params: std.json.ArrayHashMap([]const u8),
+};
+
+pub const Response = struct {
+    headers: std.json.ArrayHashMap([]const u8) = .{},
+    data: []const u8 = "",
+    status: usize = 200,
+    kv: std.json.ArrayHashMap([]const u8) = .{},
+};
 
 const InputStreamType = enum {
     stdin,
@@ -19,17 +34,8 @@ const ParseStreamOptions = struct {
     input_stream: InputStream = .stdin,
 };
 
-const Request = struct {
-    url: []const u8,
-    method: Method,
-    body: []const u8,
-    headers: std.json.ArrayHashMap([]const u8),
-    kv: std.json.ArrayHashMap([]const u8),
-    params: std.json.ArrayHashMap([]const u8),
-};
-
-/// Caller owns the memory
-pub fn parseStream(allocator: std.mem.Allocator, options: ParseStreamOptions) ParseStreamError!std.json.Parsed(Request) {
+/// Caller must call deinit on the returned result to free the associated memory
+pub inline fn parseStream(allocator: std.mem.Allocator, options: ParseStreamOptions) ParseStreamError!std.json.Parsed(Request) {
     var input = std.ArrayList(u8).init(allocator);
     defer input.deinit();
 
@@ -51,59 +57,6 @@ pub fn parseStream(allocator: std.mem.Allocator, options: ParseStreamOptions) Pa
     ) catch ParseStreamError.Unknown;
 }
 
-const FormatResponseOptions = struct {
-    data: []const u8,
-    status: usize,
-    headers: *Headers,
-    storage: *std.StringHashMap([]const u8),
-};
-
-const FormatResponseError = error{
-    Unknown,
-    OutOfMemory,
-};
-
-pub fn formatResponse(allocator: std.mem.Allocator, options: FormatResponseOptions) FormatResponseError![]const u8 {
-    var buf = std.ArrayList(u8).init(allocator);
-    defer buf.deinit();
-
-    var w = std.json.writeStream(buf.writer(), .{ .whitespace = .minified });
-    {
-        try w.beginObject();
-
-        try w.objectField("data");
-        try w.write(std.json.Value{ .string = options.data });
-
-        try w.objectField("status");
-        try w.write(options.status);
-
-        {
-            var o = std.json.ObjectMap.init(allocator);
-            defer o.deinit();
-
-            for (options.headers.list.items) |entry| {
-                try o.put(entry.name, .{ .string = entry.value });
-            }
-
-            try w.objectField("headers");
-            try w.write(std.json.Value{ .object = o });
-        }
-
-        {
-            var o = std.json.ObjectMap.init(allocator);
-            defer o.deinit();
-
-            var it = options.storage.iterator();
-            while (it.next()) |entry| {
-                try o.put(entry.key_ptr.*, .{ .string = entry.value_ptr.* });
-            }
-
-            try w.objectField("kv");
-            try w.write(std.json.Value{ .object = o });
-        }
-
-        try w.endObject();
-    }
-
-    return buf.toOwnedSlice() catch FormatResponseError.Unknown;
+pub inline fn writeResponse(response: Response, writer: anytype) !void {
+    try std.json.stringify(response, .{}, writer);
 }
