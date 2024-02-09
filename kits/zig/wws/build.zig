b6d6e3fa8089364bacdcbd7c6bf100b417fb4e8a
sea-grass <dev.seagrass@gmail.com>
2024-02-09 10:24:24 -0500

    chore: Expose `addWorker` from Zig wws SDK

--- kits/zig/wws/build.zig
+++ kits/zig/wws/build.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
-pub const Var = struct { []const u8, []const u8 };
+const module_name = "wws";
 
 pub fn build(b: *std.Build) void {
     _ = b.standardTargetOptions(.{});
     _ = b.standardOptimizeOption(.{});
 
-    const module = b.addModule("wws", .{
+    const module = b.addModule(module_name, .{
         .root_source_file = .{ .path = "src/wws.zig" },
         .target = getTarget(b),
     });
@@ -14,6 +14,7 @@ pub fn build(b: *std.Build) void {
     _ = module;
 }
 
+// Returns the wasm32-wasi target
 pub inline fn getTarget(b: *std.Build) std.Build.ResolvedTarget {
     return b.resolveTargetQuery(.{
         .cpu_arch = .wasm32,
@@ -28,19 +29,114 @@ pub const WwsLibOptions = struct {
     imports: []const std.Build.Module.Import = &.{},
 };
 
-pub fn addExecutable(b: *std.Build, options: WwsLibOptions) *std.Build.Step.Compile {
+pub const EnvVar = struct {
+    name: []const u8,
+    value: []const u8,
+};
+
+pub const WwsKv = struct {
+    namespace: []const u8,
+};
+
+pub const WwsFeatures = struct {
+    vars: ?[]const EnvVar = null,
+    kv: ?WwsKv = null,
+};
+
+pub const WwsWorker = struct {
+    exe: *std.Build.Step.Compile,
+    options: ModuleOptions,
+
+    pub fn addToWriteFiles(self: WwsWorker, b: *std.Build, wf: *std.Build.Step.WriteFile) !void {
+        _ = wf.addCopyFile(self.exe.getEmittedBin(), b.fmt("{s}.wasm", .{self.options.path}));
+
+        const config = try self.formatConfig(b.allocator);
+        defer b.allocator.free(config);
+        _ = wf.add(b.fmt("{s}.toml", .{self.options.path}), config);
+    }
+
+    // Very naively write the worker config to a string. Environment variable values
+    // and KV namespace name are automatically quoted.
+    pub fn formatConfig(self: WwsWorker, allocator: std.mem.Allocator) ![]u8 {
+        var buf = std.ArrayList(u8).init(allocator);
+        errdefer buf.deinit();
+
+        try buf.writer().print(
+            \\name = "{s}"
+            \\version = "1"
+            \\
+        ,
+            .{
+                self.options.name,
+            },
+        );
+
+        if (self.options.features.vars) |vars| {
+            try buf.writer().print(
+                \\[vars]
+                \\
+            ,
+                .{},
+            );
+            for (vars) |v| {
+                try buf.writer().print(
+                    \\{s} = "{s}"
+                    \\
+                ,
+                    .{ v.name, v.value },
+                );
+            }
+        }
+
+        if (self.options.features.kv) |kv| {
+            try buf.writer().print(
+                \\[data]
+                \\[data.kv]
+                \\namespace = "{s}"
+                \\
+            ,
+                .{
+                    kv.namespace,
+                },
+            );
+        }
+
+        return try buf.toOwnedSlice();
+    }
+};
+
+pub const ModuleOptions = struct {
+    // The name of the module, written to the worker config
+    name: []const u8,
+    root_source_file: std.Build.LazyPath,
+    // The relative path of the module and its worker config.
+    // E.g. `.{ .path = "api/[...params]" }` will write to
+    // `api/[...params].wasm` and `api/[...params].toml`.
+    path: []const u8,
+    // The worker features/config including KV namespace,
+    // environment variables, and more. These are written
+    // to the worker config.
+    features: WwsFeatures = .{},
+    // You must provide the wws dependency to ensure the
+    // wws module is linked to your project.
+    wws: *std.Build.Dependency,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+};
+
+pub fn addWorker(b: *std.Build, options: ModuleOptions) !WwsWorker {
     const exe = b.addExecutable(.{
         .name = options.name,
         .root_source_file = options.root_source_file,
-        .target = getTarget(b),
+        .target = options.target,
         .optimize = options.optimize,
     });
 
     exe.wasi_exec_model = .reactor;
+    exe.root_module.addImport("wws", options.wws.module(module_name));
 
-    for (options.imports) |import| {
-        exe.root_module.addImport(import.name, import.module);
-    }
-
-    return exe;
+    return .{
+        .exe = exe,
+        .options = options,
+    };
 }
